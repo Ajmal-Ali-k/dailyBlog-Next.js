8ca5c9e064b5dc357795f038ef98808deb078d4a
Ajmal-Ali-k <ajmalali749@gmail.com>
2023-10-20 09:12:16 +0530

    navbar compleated

--- src/components/themeToggle/ThemeToggle.jsx
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -8,7 +8,14 @@ const ThemeToggle = () => {
   const { theme, toggle } = useContext(ThemeContext);
 
   return (
-    <div className={styles.container}  onClick={toggle}>
+    <div className={styles.container} 
+    style={
+      theme === "dark"
+        ? {  backgroundColor: "white" }
+        : {  backgroundColor: "#0f172a" }
+    }
+    
+    onClick={toggle}>
       <Image src="/moon.png" alt="moon" width={14} height={14} />
       <div
         className={styles.ball}
